fe86cd64675aee48a3fcefe3f0310a4fe18eba20
yuyahhh <yuyasakai6120@gmail.com>
2019-12-14 21:39:17 +0900

    definition_routing_controller_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
