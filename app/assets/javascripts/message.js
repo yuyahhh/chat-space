8399e9c714d2076f7322679b08996579522a1d75
yuyahhh <yuyasakai6120@gmail.com>
2020-01-11 15:04:09 +0900

    debug_deletion

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -42,7 +42,7 @@ $(function(){
 
   var reloadMessages = function() {
     last_message_id = $('.chat-main__messages__message').last().data("message-id");
-    console.log(last_message_id);
+    
     $.ajax({
       url: "api/messages",
       type: 'get',
@@ -50,7 +50,6 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log(messages);
       if (messages.length !== 0) {
         var insertHTML = '';
         $.each(messages, function(i, message) {
