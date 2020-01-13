d2ffa598ef16feabebbecaa9cdb6fcae1b67b891
yuyahhh <yuyasakai6120@gmail.com>
2020-01-13 18:31:25 +0900

    s3_environmental_preference

--- Gemfile
+++ Gemfile
@@ -67,6 +67,7 @@ gem 'devise'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'jquery-rails'
+gem 'fog-aws'
 
 group :production do
   gem 'unicorn', '5.4.1'
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, 'chat-space'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yuyahhh/chat-space.git'
 
-# バージョンが変わっても共通で参照するディレクトリを指定
+# バージョンが変わっても共通で参照するディレクトリを指定git config --global init.templatedir '~/.git-templates/git-secrets'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
@@ -25,10 +25,24 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :linked_files, %w{ config/secrets.yml }
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
